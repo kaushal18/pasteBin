0986083a9f7e3c34ed9ae635b55c3a99dc44d40a
kaushal yadav <kaushal.y@somaiya.edu>
2020-12-05 13:00:16 +0530

    copy link to clipboard

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const queryParam = parsedUrl.searchParams.get("c");
 document.title += ` / ${queryParam}`;
 
 const textArea = document.querySelector("#content");
+const copyLink = document.querySelector(".copyLink");
 const socket = io("http://localhost:5000", { query: `token=${queryParam}` });
 
 socket.on("connect", () => {
@@ -17,4 +18,16 @@ socket.on("message", (message) => {
 // listner for changes in textArea
 textArea.addEventListener("input", (event) => {
   socket.emit("message", event.target.value);
+});
+
+copyLink.addEventListener("click", () => {
+  const url = window.location.href;
+  navigator.clipboard.writeText(url).then(
+    function () {
+      console.log("copied");
+    },
+    function () {
+      console.log("failed");
+    }
+  );
 });
