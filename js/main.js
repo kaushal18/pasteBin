618de410684d373e6a82391ef2ec7ec76c15ee22
kaushal yadav <kaushal.y1801@gmail.com>
2020-12-27 20:18:04 +0530

    undo redo implementation

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ const debouceParams = {
   debouceParams: 300,
 };
 
+// in memory stack
+let undoStack = [];
+let redoStack = [];
+
 // update title of page
 document.title += ` / ${queryParam}`;
 // make socket connection
@@ -22,16 +26,24 @@ socket.on("connect", () => {
 // listen for content change from server
 socket.on("message", (message) => {
   textArea.value = message;
+  undoStack.length = 0;
+  redoStack.length = 0;
 });
 
-// listner for changes in textArea
-textArea.addEventListener("input", (event) => {
+function emitMessage(content) {
   // debounce the request to server
   clearTimeout(debouceParams.timerId);
 
   debouceParams.timerId = setTimeout(() => {
-    socket.emit("message", event.target.value);
+    socket.emit("message", content);
   }, debouceParams.delay);
+}
+
+// listner for changes in textArea
+textArea.addEventListener("input", (event) => {
+  const content = event.target.value;
+  undoStack.push(content.trim());
+  emitMessage(content);
 });
 
 // copy link to clipboard
@@ -39,14 +51,13 @@ copyLinkButton.addEventListener("click", () => {
   const url = window.location.href;
   navigator.clipboard.writeText(url).then(
     () => (copyLinkButton.innerHTML = "Copied!"),
-    () => alert("error, please copy the link from browser search bar")
+    () => alert("error, please copy the link from browser url bar")
   );
 });
 
 // migrate to new url
 migrateUrlBtn.addEventListener("click", () => {
   const newToken = prompt("enter a new url");
-  let isError = false;
   if (newToken === null) return;
   if (newToken.trim() === "") return alert("invalid url");
 
@@ -80,4 +91,30 @@ migrateUrlBtn.addEventListener("click", () => {
 newUrlBtn.addEventListener("click", () => {
   window.location.href =
     window.location.protocol + "//" + window.location.host + `?c=`;
+});
+
+// listen for undo and redo
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key === "z") {
+    event.preventDefault();
+    const currVal = undoStack.pop();
+    if (currVal) {
+      redoStack.push(currVal);
+      const undoVal = undoStack[undoStack.length - 1];
+      if (undoVal) {
+        textArea.value = undoVal;
+        emitMessage(textArea.value);
+      }
+    }
+    console.log(undoStack);
+  } else if (event.ctrlKey && event.key === "y") {
+    event.preventDefault();
+    const redoVal = redoStack.pop();
+    if (redoVal) {
+      textArea.value = redoVal;
+      undoStack.push(redoVal);
+      emitMessage(textArea.value);
+    }
+    // console.log(redoStack);
+  }
 });
